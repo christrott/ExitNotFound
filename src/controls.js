8f4d1affc77a52615c5c6bcba2607be0258ede29
Chris Trott <christrott86@gmail.com>
2020-09-12 15:06:34 +1000

    Added touch controls

--- src/controls.js
+++ src/controls.js
@@ -5,6 +5,10 @@ module.exports = class Controls {
       this.showMaze = false;
       document.addEventListener('keydown', this.onKey.bind(this, true), false);
       document.addEventListener('keyup', this.onKey.bind(this, false), false);
+
+      document.addEventListener('touchstart', this.onTouch.bind(this), false);
+      document.addEventListener('touchmove', this.onTouch.bind(this), false);
+      document.addEventListener('touchend', this.onTouchEnd.bind(this), false);
     }
   
     onKey(val, e) {
@@ -18,5 +22,26 @@ module.exports = class Controls {
       this.states[state] = val;
       e.preventDefault && e.preventDefault();
       e.stopPropogation && e.stopPropogation();
+    }
+
+    onTouch(e) {
+      var t = e.touches[0];
+      this.onTouchEnd(e);
+      console.log(t.pageX, t.pageY);
+      if (t.pageY < window.innerHeight * 0.5) {
+        this.onKey(true, { keyCode: 38 });
+      } else if (t.pageY > window.innerHeight * 0.8) {
+        this.onKey(true, { keyCode: 77 });
+      } else if (t.pageX < window.innerWidth * 0.5) {
+        this.onKey(true, { keyCode: 37 });
+      } else if (t.pageY > window.innerWidth * 0.5) {
+        this.onKey(true, { keyCode: 39 });
+      }
+    }
+
+    onTouchEnd(e) {
+      this.states = { 'left': false, 'right': false, 'forward': false, 'backward': false };
+      e.preventDefault();
+      e.stopPropagation();
     }
   }
